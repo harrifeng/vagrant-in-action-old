c79325638f13517b34d924178e82b31520f6dcbc
Haoran Feng <haoran.feng@alo7.com>
2017-04-25 17:18:42 +0800

    before glance

--- openstack-in-action/controller.sh
+++ openstack-in-action/controller.sh
@@ -1,5 +1,8 @@
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install rabbitmq-server python-mysqldb mysql-server
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install rabbitmq-server \
+     python-mysqldb \
+     keystone \
+     mysql-server
 sudo rabbitmqctl change_password guest openstack1
 echo 'rabbitmq status------------------->'
 sudo rabbitmqctl status
@@ -7,3 +10,5 @@ sudo sed -i "s/^bind\-address.*/bind-address = 0.0.0.0/g" /etc/mysql/my.cnf
 sudo service mysql restart
 echo 'mysql status---------------------->'
 sudo service mysql status
+echo 'keystone status---------------------->'
+id keystone
---
+++ openstack-in-action/keystone.auth.sh
@@ -0,0 +1,10 @@
+#This file contains environmental variables used to access Keystone
+# Host address
+HOST_IP=192.168.2.50 #The Management Address
+# Keystone definitions
+KEYSTONE_REGION=RegionOne
+ADMIN_PASSWORD=admin_pass
+SERVICE_PASSWORD=service_pass
+export SERVICE_TOKEN="ADMIN"
+export SERVICE_ENDPOINT="http://192.168.2.50:35357/v2.0"
+SERVICE_TENANT_NAME=service
