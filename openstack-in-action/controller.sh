f36e03c9887cad530236a498013ee0acb02170a4
Haoran Feng <haoran.feng@alo7.com>
2017-04-25 16:27:29 +0800

    successfully install the two software

---
+++ openstack-in-action/controller.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install rabbitmq-server python-mysqldb mysql-server
+sudo rabbitmqctl change_password guest openstack1
+echo 'rabbitmq status------------------->'
+sudo rabbitmqctl status
+sudo sed -i "s/^bind\-address.*/bind-address = 0.0.0.0/g" /etc/mysql/my.cnf
+sudo service mysql restart
+echo 'mysql status---------------------->'
+sudo service mysql status
---
+++ openstack-in-action/prepare.sh
@@ -0,0 +1,1 @@
+sudo wget http://mirrors.163.com/.help/sources.list.trusty -O /etc/apt/sources.list
