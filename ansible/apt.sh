c98802453409d3e75cb9b135ddad3224b7687d07
Haoran Feng <haoran.feng@alo7.com>
2017-05-02 13:09:47 +0800

    we should not update the apt, problems happened

--- ansible/apt.sh
+++ ansible/apt.sh
@@ -1,11 +1,11 @@
-echo "
-deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse" | sudo tee /etc/apt/sources.list
+# echo "
+# deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse" | sudo tee /etc/apt/sources.list
--- ansible/pre.sh
+++ ansible/pre.sh
@@ -1,1 +1,2 @@
+sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install aptitude build-essential git ntp ntpdate openssh-server python-dev
