b4e1041557d3aa8528bc1bb53f487b4f51c2d769
Haoran Feng <haoran.feng@alo7.com>
2017-05-09 10:56:12 +0800

    new start

--- install-guid/controller.sh
+++ install-guid/controller.sh
@@ -32,19 +32,16 @@ echo "
 # NTP implementation Chrony
 echo '----------------------------'
 sudo apt-get install -y chrony
-
-
 echo "
 allow 10.0.0.0/24
 " | sudo tee -a /etc/chrony/chrony.conf
-
 sudo service chrony restart
+
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y cloud-archive:ocata
 
 sudo apt update
 sudo apt-get install -y python-openstackclient
-
 sudo apt-get install -y mariadb-server python-pymysql
 
 echo "
@@ -55,12 +52,12 @@ innodb_file_per_table = on
 max_connections = 4096
 collation-server = utf8_general_ci
 character-set-server = utf8" | sudo tee -a /etc/mysql/mariadb.conf.d/99-openstack.cnf
-
 sudo service mysql restart
 sudo mysql -uroot -h localhost -e "SHOW DATABASES"
+
 sudo apt-get install -y rabbitmq-server
-rabbitmqctl add_user openstack welcome
-rabbitmqctl set_permissions openstack ".*" ".*" ".*"
+sudo rabbitmqctl add_user openstack welcome
+sudo rabbitmqctl set_permissions openstack ".*" ".*" ".*"
 sudo apt-get install -y memcached python-memcache
 
 sudo sed -i "s/-l 127.0.0.1/-l 10.0.0.11/g" /etc/memcached.conf
@@ -70,6 +67,9 @@ sudo mysql -uroot -h localhost -e "CREATE DATABASE keystone"
 sudo mysql -uroot -h localhost -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'welcome';"
 sudo mysql -uroot -h localhost -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'welcome';"
 sudo apt-get install -y keystone
+
+# try sed here
+
 
 # echo "
 # connection = mysql+pymysql://keystone:welcome@controller/keystone
