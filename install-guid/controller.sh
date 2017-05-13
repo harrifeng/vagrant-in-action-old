57fa09feaee41c5ad02fb1422a3bda67ea4102df
Haoran Feng <haoran.feng@alo7.com>
2017-05-13 18:36:34 +0800

    try again

--- install-guid/controller.sh
+++ install-guid/controller.sh
@@ -30,15 +30,11 @@ echo "
 # object2
 10.0.0.52 object2" | sudo tee -a /etc/hosts
 
-
-echo '-----Install Chrony-----------------------'
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install chrony
-echo '-----Install Chrony-----------------------'
 
+echo '-------------------------------------------------------------------------'
 # page 15
-
 CONF_CHRONY=/etc/chrony/chrony.conf
-
 echo "
 server 0.amazon.pool.ntp.org iburst
 allow 10.0.0.0/24
@@ -46,16 +42,17 @@ allow 10.0.0.0/24
 
 sudo service chrony restart
 
-echo ${CONF_CHRONY} '------------------------------------------------------>>'
+echo '----------------------------------------------------------------------->>'
+echo ${CONF_CHRONY}
+echo '----------------------------------------------------------------------->>'
 cat ${CONF_CHRONY}
-echo ${CONF_CHRONY} '------------------------------------------------------>>'
+echo '-----------------------------------------------------------------------<<'
+echo ${CONF_CHRONY}
+echo '-----------------------------------------------------------------------<<'
 
-# page 16
-echo 'Run > chronyc sources-----------------------------------'
 chronyc sources
-echo '--------------------------------------------------------'
 
-
+echo '-------------------------------------------------------------------------'
 # page 17 Distributions release Openstack packages as part of the distribution or
 # using other methods because of differing release schedules.
 # Perform these procedures on all nodes.
@@ -65,10 +62,10 @@ sudo add-apt-repository -y cloud-archive:ocata
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python-openstackclient
 
-
 # The database typically runs on the controller node.
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mariadb-server python-pymysql
 
+echo '-------------------------------------------------------------------------'
 # page 18
 echo "
 [mysqld]
@@ -79,11 +76,18 @@ max_connections = 4096
 collation-server = utf8_general_ci
 character-set-server = utf8" | sudo tee -a /etc/mysql/mariadb.conf.d/99-openstack.cnf
 
-sudo service mysql restart
-echo 'install database------------------------------'
-sudo mysql -uroot -h localhost -e "SHOW DATABASES"
-echo '----------------------------------------------'
+echo '----------------------------------------------------------------------->>'
+echo /etc/mysql/mariadb.conf.d/99-openstack.cnf
+echo '----------------------------------------------------------------------->>'
+cat /etc/mysql/mariadb.conf.d/99-openstack.cnf
+echo '-----------------------------------------------------------------------<<'
+echo /etc/mysql/mariadb.conf.d/99-openstack.cnf
+echo '-----------------------------------------------------------------------<<'
 
+sudo service mysql restart
+sudo mysql -uroot -h localhost -e "SHOW DATABASES"
+
+echo '-------------------------------------------------------------------------'
 # page 19: the message queue runs on the controller node.
 #
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install rabbitmq-server
@@ -92,23 +96,28 @@ sudo rabbitmqctl add_user openstack ${RABBIT_PASS}
 sudo rabbitmqctl set_permissions openstack ".*" ".*" ".*"
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install memcached python-memcache
+
 CONF_MEMCACHE=/etc/memcached.conf
 sudo sed -i "s/-l 127.0.0.1/-l 10.0.0.11/g" ${CONF_MEMCACHE}
 sudo service memcached restart
-echo ${CONF_MEMCACHE} '---------------------------------------'
-cat ${CONF_MEMCACHE}
-echo ${CONF_MEMCACHE} '---------------------------------------'
 
+echo '----------------------------------------------------------------------->>'
+echo ${CONF_MEMCACHE}
+echo '----------------------------------------------------------------------->>'
+cat ${CONF_MEMCACHE}
+echo '-----------------------------------------------------------------------<<'
+echo ${CONF_MEMCACHE}
+echo '-----------------------------------------------------------------------<<'
+
+echo '-------------------------------------------------------------------------'
 # Identity service page 20: A centralized server provides authentication and authorization
 # services using a RESTful interface
 
 # page 21
-echo '--------------start-----------mysql----------------------------'
 DB_PASS=welcome
 sudo mysql -uroot -h localhost -e "CREATE DATABASE keystone"
 sudo mysql -uroot -h localhost -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '${DB_PASS}';"
 sudo mysql -uroot -h localhost -e "GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '${DB_PASS}';"
-echo '--------------end-------------mysql----------------------------'
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install keystone
 
@@ -121,9 +130,13 @@ sudo sed -i "s|#provider.*|provider = fernet|g" ${CONF_KEYSTONE}
 # page 22: Populate the Identity service database
 sudo su -s /bin/sh -c "keystone-manage db_sync" keystone
 
-echo ${CONF_KEYSTONE} '-----------------------'
+echo '----------------------------------------------------------------------->>'
+echo ${CONF_KEYSTONE}
+echo '----------------------------------------------------------------------->>'
 cat ${CONF_KEYSTONE}
-echo '----------------------------------------'
+echo '-----------------------------------------------------------------------<<'
+echo ${CONF_KEYSTONE}
+echo '-----------------------------------------------------------------------<<'
 
 # Initialize Fernet key repositories:
 sudo keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
@@ -136,7 +149,8 @@ sudo keystone-manage bootstrap --bootstrap-password ${ADMIN_PASS} \
                 --bootstrap-public-url http://controller:5000/v3/ \
                 --bootstrap-region-id RegionOne
 
-# Configure the Apache HTTP server page 22
+echo '-------------------------------------------------------------------------'
+# page 22 Configure the Apache HTTP server
 echo "ServerName controller" | sudo tee -a /etc/apache2/apache2.conf
 # Restart the Apache service and remove the default SQLite database
 sudo service apache2 restart
@@ -151,12 +165,14 @@ export OS_AUTH_URL=http://controller:35357/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_IMAGE_API_VERSION=2
 
-echo 'test----------openstack urlj---------------'
+echo '-------------------------------------------------------------------------'
+echo '> openstack project list'
+echo '-------------------------------------------------------------------------'
 openstack project list
-echo '-------------------------------------------'
 
-# page 23: This guide uses a service project that contains a unique user for each service that
-# you add to your environment. Create the service project
+echo '-------------------------------------------------------------------------'
+# page 23: This guide uses a service project that contains a unique user for each
+# service that you add to your environment. Create the service project
 openstack project create --domain default --description "Service Project" service
 
 # Regular(non-admin) tasks should use an unprivileged project and user. As an example,
@@ -174,20 +190,21 @@ openstack role create user
 # add role to the demo project and user
 openstack role add --project demo --user demo user
 
-echo 'test token issue-----------------------'
+echo '-------------------------------------------------------------------------'
+echo '> openstack token issue'
+echo '-------------------------------------------------------------------------'
 openstack token issue
-echo '---------------------------------------'
 
+echo '-------------------------------------------------------------------------'
 # page 26: Image service
-#
 # glance
 DB_PASS=welcome
 sudo mysql -uroot -h localhost -e "CREATE DATABASE glance"
 sudo mysql -uroot -h localhost -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY '${DB_PASS}';"
 sudo mysql -uroot -h localhost -e "GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY '${DB_PASS}';"
 
+echo '-------------------------------------------------------------------------'
 # page 28: To create the service credentials, complete following steps
-
 # 1.create glance user
 openstack user create --domain default --password welcome glance
 # 2.Add the admin role to the glance user and service project
@@ -200,7 +217,8 @@ openstack endpoint create --region RegionOne image public http://controller:9292
 openstack endpoint create --region RegionOne image internal http://controller:9292
 openstack endpoint create --region RegionOne image admin http://controller:9292
 
-# Install packages page 30
+echo '-------------------------------------------------------------------------'
+# page 30 Install packages
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install glance
 
 CONF_GLANCE_API=/etc/glance/glance-api.conf
@@ -209,10 +227,10 @@ sudo sed -i "s|#auth_uri.*|auth_uri = http://controller:5000|g" ${CONF_GLANCE_AP
 sudo sed -i "/auth_uri = /a auth_url = http://controller:35357" ${CONF_GLANCE_API}
 sudo sed -i "s|#memcached_servers.*|memcached_servers = controller:11211|g" ${CONF_GLANCE_API}
 sudo sed -i "s|#auth_type.*|auth_type = password|g" ${CONF_GLANCE_API}
-sudo sed -i "/auth_type =/a project_domain_name=default \
-user_domain_name = default \
-project_name = service \
-username = glance \
+sudo sed -i "/auth_type =/a project_domain_name=default \n
+user_domain_name = default \n
+project_name = service \n
+username = glance \n
 password = welcome" ${CONF_GLANCE_API}
 sudo sed -i "s|#flavor.*|flavor = keystone|g" ${CONF_GLANCE_API}
 sudo sed -i "s|#stores.*|stores = file,http|g" ${CONF_GLANCE_API}
@@ -220,9 +238,13 @@ sudo sed -i "s|#default_store.*|default_store = file|g" ${CONF_GLANCE_API}
 sudo sed -i "s|#filesystem_store_datadir.*|filesystem_store_datadir = /var/lib/glance/images/|g" ${CONF_GLANCE_API}
 
 
-echo ${CONF_GLANCE_API} '--------------------------'
+echo '----------------------------------------------------------------------->>'
+echo ${CONF_GLANCE_API}
+echo '----------------------------------------------------------------------->>'
 cat ${CONF_GLANCE_API}
-echo '---------------------------------------------'
+echo '-----------------------------------------------------------------------<<'
+echo ${CONF_GLANCE_API}
+echo '-----------------------------------------------------------------------<<'
 
 CONF_GLANCE_REG=/etc/glance/glance-registry.conf
 sudo sed -i "s|#connection.*|connection = mysql+pymysql://glance:${DB_PASS}@controller/glance|g" ${CONF_GLANCE_REG}
@@ -231,23 +253,37 @@ sudo sed -i "/auth_uri/a auth_url = http://controller:35357" ${CONF_GLANCE_REG}
 
 sudo sed -i "s|#memcached_servers.*|memcached_servers=controller:11211|g" ${CONF_GLANCE_REG}
 sudo sed -i "s|#auth_type.*|auth_type = password|g" ${CONF_GLANCE_REG}
-sudo sed -i "/auth_type/a project_domain_name=default \
-user_domain_name = default \
-project_name = service \
-username = glance \
+sudo sed -i "/auth_type/a project_domain_name=default \n
+user_domain_name = default \n
+project_name = service \n
+username = glance \n
 password = welcome" ${CONF_GLANCE_REG}
 sudo sed -i "s|#flavor.*|flavor = keystone|g" ${CONF_GLANCE_REG}
 
-echo ${CONF_GLANCE_REG} '--------------------------'
+echo '----------------------------------------------------------------------->>'
+echo ${CONF_GLANCE_REG}
+echo '----------------------------------------------------------------------->>'
 cat ${CONF_GLANCE_REG}
-echo '---------------------------------------------'
+echo '-----------------------------------------------------------------------<<'
+echo ${CONF_GLANCE_REG}
+echo '-----------------------------------------------------------------------<<'
 
+echo '-------------------------------------------------------------------------'
+echo '>sudo su -s /bin/sh -c "glance-manage db_sync" glance'
+echo 'Note: Ignore any deprecation messages in this output.'
+echo '-------------------------------------------------------------------------'
 sudo su -s /bin/sh -c "glance-manage db_sync" glance
 sudo service glance-registry restart
 sudo service glance-api restart
-#
-# openstack image create "cirros" --file cirros-0.3.5-x86_64-disk.img --disk-format qcow2 --container-format bare --public
-# openstack image list
+
+echo '-------------------------------------------------------------------------'
+# page 31 : Verify operation
+openstack image create "cirros" --file /vagrant/cirros-0.3.5-x86_64-disk.img --disk-format qcow2 --container-format bare --public
+echo '-------------------------------------------------------------------------'
+echo 'openstack image list'
+echo '-------------------------------------------------------------------------'
+openstack image list
+
 #
 # # Install and configure controller node
 # # This section describes how to install and configure the Compute service, code-named nova, on the controller node
