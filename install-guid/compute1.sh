57aaf151bdf9a644e0149cc5040556cd84f9db37
Haoran Feng <haoran.feng@alo7.com>
2017-05-16 12:52:19 +0800

    new try

--- install-guid/compute1.sh
+++ install-guid/compute1.sh
@@ -55,49 +55,48 @@ echo '--------------------------------------------------------'
 # Perform these procedures on all nodes.
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common
 sudo add-apt-repository -y cloud-archive:ocata
-
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python-openstackclient
 
 
-# # sudo apt-get install -y software-properties-common
-# # sudo add-apt-repository -y cloud-archive:ocata
-# #
-# # sudo apt-get update
-# # sudo apt-get install -y python-openstackclient
-#
-#
 # # Install and configure: This section describes how to install and configure the Compute service on a compute node
-# sudo apt-get install -y nova-compute
-#
-# CONF_NOVA=/etc/nova/nova.conf
-# sudo sed -i "s|#transport_url=<None>|transport_url = rabbit://openstack:welcome@controller|g" ${CONF_NOVA}
-# sudo sed -i "s|#auth_strategy=keystone|auth_strategy=keystone|g" ${CONF_NOVA}
-# sudo sed -i "s|#auth_uri = <None>|auth_uri = http://controller:5000\nauth_url = http://controller:35357|g" ${CONF_NOVA}
-# sudo sed -i "s|#memcached_servers = <None>|controller:11211|g" ${CONF_NOVA}
-# sudo sed -i "s|#auth_type = <None>|auth_type = password\nproject_domain_name=default\nuser_domain_name = default\nproject_name = service\nusername = glance\npassword = welcome|g" ${CONF_NOVA}
-# sudo sed -i "s|#project_domain_name=<None>|project_domain_name=default|g" ${CONF_NOVA}
-# sudo sed -i "s|#user_domain_name=<None>|user_domain_name=default|g" ${CONF_NOVA}
-# sudo sed -i "s|#project_name=<None>|project_name=service|g" ${CONF_NOVA}
-# sudo sed -i "s|#username =|username = nova|g" ${CONF_NOVA}
-# sudo sed -i "s|#password =|password = welcome|g" ${CONF_NOVA}
-# sudo sed -i "s|^#my_ip.*|my_ip = 10.0.0.31|g" ${CONF_NOVA}
-# sudo sed -i "s|#use_neutron=true|use_neutron=true|g" ${CONF_NOVA}
-# sudo sed -i "s|#firewall_driver=<None>|firewall_driver=nova.virt.firewall.NoopFirewallDriver|g" ${CONF_NOVA}
-# # vnc is not configured here
-# # glance
-# sudo sed -i "s|#api_servers=<None>|api_servers= http://controller:9292|g" ${CONF_NOVA}
-# sudo sed -i "s|lock_path=/var/lock/nova|lock_path=/var/lock/nova/tmp|g" ${CONF_NOVA}
-# # rm log_dir
-#
-# sudo sed -i "s|^os_region_name.*|os_region_name = RegionOne|g" ${CONF_NOVA}
-# sudo sed -i "s|#project_domain_name=<None>|project_domain_name=Default|g" ${CONF_NOVA}
-# sudo sed -i "s|#project_name=<None>|project_name=service|g" ${CONF_NOVA}
-#
-# sudo sed -i "s|#user_domain_name=<None>|user_domain_name=Default|g" ${CONF_NOVA}
-# sudo sed -i "s|#auth_url=<None>|auth_url=http://controller:35357/v3|g" ${CONF_NOVA}
-# sudo sed -i "s|#username =|username = placement|g" ${CONF_NOVA}
-# sudo sed -i "s|#password =|password = welcome|g" ${CONF_NOVA}
-#
-# sudo sed -i "s|virt_type=.*|virt_type=qemu|g" /etc/nova/nova-compute.conf
-# sudo service nova-compute restart
+# Page 41: Install the packages
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install nova-compute
+
+CONF_NOVA=/etc/nova/nova.conf
+sudo sed -i "/\[DEFAULT\]$/a transport_url = rabbit://openstack:welcome@controller" ${CONF_NOVA}
+sudo sed -i "/\[api\]$/a auth_strategy=keystone" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a auth_uri = http://controller:5000" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a auth_url = http://controller:35357" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a memcached_servers = controller:11211" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a auth_type = password" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a project_domain_name = default" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a user_domain_name = default" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a project_name = service" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a username = nova" ${CONF_NOVA}
+sudo sed -i "/\[keystone_authtoken\]$/a password = welcome" ${CONF_NOVA}
+sudo sed -i "/\[DEFAULT\]$/a my_ip = 10.0.0.11" ${CONF_NOVA}
+sudo sed -i "/\[DEFAULT\]$/a use_neutron = True" ${CONF_NOVA}
+sudo sed -i "/\[DEFAULT\]$/a firewall_driver=nova.virt.firewall.NoopFirewallDriver" ${CONF_NOVA}
+sudo sed -i "/\[vnc\]$/a enabled = true" ${CONF_NOVA}
+sudo sed -i "/\[vnc\]$/a vncserver_listen = 0.0.0.0" ${CONF_NOVA}
+sudo sed -i "/\[vnc\]$/a vncserver_proxyclient_address = 10.0.0.11" ${CONF_NOVA}
+sudo sed -i "/\[vnc\]$/a novncproxy_base_url = http://controller:6080/vnc_auto.html" ${CONF_NOVA}
+sudo sed -i "/\[glance\]$/a api_servers = http://controller:9292" ${CONF_NOVA}
+sudo sed -i "/\[oslo_concurrency\]$/a lock_path=/var/lock/nova/tmp" ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a os_region_name = RegionOne             " ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a project_domain_name = Default          " ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a project_name = service                 " ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a auth_type = password                   " ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a user_domain_name = Default             " ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a auth_url = http://controller:35357/v3  " ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a username = placement                   " ${CONF_NOVA}
+sudo sed -i "/\[placement\]$/a password = welcome                     " ${CONF_NOVA}
+sudo sed -i "/\[libvirt\]$/a virt_type = qemu                         " ${CONF_NOVA}
+sudo service nova-compute restart
+
+echo '-------------------------------------------------------------------------'
+echo 'log'
+echo '-------------------------------------------------------------------------'
+
+sudo cat /var/log/nova/nova-compute.log
